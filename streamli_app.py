98ad2a1c8eb7cfe9d2e91a499a519b10638890dc
GGowthamiG <145528263+GGowthamiG@users.noreply.github.com>
2023-09-20 11:19:42 +0530

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('kale,spinach & rocket Smoothie')
+streamlit.text('Hard-boiled fre-range egg')
