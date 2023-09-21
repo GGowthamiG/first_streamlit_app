a37298f53520114f97e118f0ee06f9f063fe9387
GGowthamiG <145528263+GGowthamiG@users.noreply.github.com>
2023-09-21 09:26:02 +0530

    Update streamli_app.py
    added breakfast menu header and 3 items

--- streamli_app.py
+++ streamli_app.py
@@ -3,9 +3,6 @@ streamlit.title('my parents new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
